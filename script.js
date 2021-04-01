2d8b1ee50ff89d5b9a061a79f35b22aa54e75cb6
unknown <cristiano.enke@gmail.com>
2021-04-01 18:54:13 -0300

    Adicao de sprites para a snake e a comida

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let snakeupsection = []; //This will map the direction of previous section to help the draw
+let snakedownsection = []; //This will map the direction of next section to help the draw
+
+
+var tile_sheet = new Image();
+tile_sheet.src = "img/snake-graphics.png"
+var sprite_size = 64;
+
 snake[0] = {
     x:8*box,
     y:8*box}
-    
+
 let direction = "right";
 let food = {
     x: Math.floor(Math.random()*15+1)*box,
@@ -19,14 +27,73 @@ function CreateBG() {
 
 function CreateSnake(){
     for (i=0; i < snake.length; i++){
-        context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box)
+       // context.fillStyle = "green";
+       // context.fillRect(snake[i].x, snake[i].y, box, box); 
+        
+        if(i==0 && direction == "up")
+            context.drawImage(tile_sheet, 3*64, 0*64, 64,64,snake[i].x,snake[i].y,box,box);
+        if(i==0 && direction =="down")
+            context.drawImage(tile_sheet, 4*64, 1*64, 64,64,snake[i].x,snake[i].y,box,box);
+        if(i==0 && direction =="left")
+            context.drawImage(tile_sheet, 3*64, 1*64, 64,64,snake[i].x,snake[i].y,box,box);
+        if(i==0 && direction =="right")
+            context.drawImage(tile_sheet, 4*64, 0*64, 64,64,snake[i].x,snake[i].y,box,box);    
+    
+        if(i != 0 && i != snake.length-1){
+            if (snake[i-1].x == snake[i].x && snake[i+1].x == snake[i].x){ //going up or down
+                context.drawImage(tile_sheet, 2*64, 1*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+            if (snake[i-1].y == snake[i].y &&snake[i+1].y == snake[i].y){ //going left or right
+                context.drawImage(tile_sheet, 1*64, 0*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+                // clock or anticlock movement:
+            if (snake[i-1].x > snake[i].x && snake[i-1].y == snake[i].y && snake[i+1].x == snake[i].x && snake[i+1].y > snake[i].y){
+                context.drawImage(tile_sheet, 0*64, 0*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+            if (snake[i-1].x == snake[i].x && snake[i-1].y > snake[i].y && snake[i+1].x > snake[i].x && snake[i+1].y == snake[i].y){
+                context.drawImage(tile_sheet, 0*64, 0*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+            
+            if (snake[i-1].x < snake[i].x && snake[i-1].y == snake[i].y && snake[i+1].x == snake[i].x && snake[i+1].y > snake[i].y){
+                context.drawImage(tile_sheet, 2*64, 0*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+            if (snake[i-1].x == snake[i].x && snake[i-1].y > snake[i].y && snake[i+1].x < snake[i].x && snake[i+1].y == snake[i].y){
+                context.drawImage(tile_sheet, 2*64, 0*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+
+            if (snake[i-1].x < snake[i].x && snake[i-1].y == snake[i].y && snake[i+1].x == snake[i].x && snake[i+1].y < snake[i].y){
+                context.drawImage(tile_sheet, 2*64, 2*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+            if (snake[i-1].x == snake[i].x && snake[i-1].y < snake[i].y && snake[i+1].x < snake[i].x && snake[i+1].y == snake[i].y){
+                context.drawImage(tile_sheet, 2*64, 2*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+
+            if (snake[i-1].x == snake[i].x && snake[i-1].y < snake[i].y && snake[i+1].x > snake[i].x && snake[i+1].y == snake[i].y){
+                context.drawImage(tile_sheet, 0*64, 1*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+            if (snake[i-1].x > snake[i].x && snake[i-1].y == snake[i].y && snake[i+1].x == snake[i].x && snake[i+1].y < snake[i].y){
+                context.drawImage(tile_sheet, 0*64, 1*64, 64, 64,snake[i].x,snake[i].y,box,box)
+            }
+
+        }
+        // tail:
+        if(snake.length !=1 && i==snake.length-1 && snake[i-1].x == snake[i].x && snake[i-1].y < snake[i].y ) //going up
+            context.drawImage(tile_sheet, 3*64, 2*64, 64, 64,snake[i].x,snake[i].y,box,box);  
+        if(snake.length !=1 && i==snake.length-1 && snake[i-1].x == snake[i].x && snake[i-1].y > snake[i].y ) //going down
+            context.drawImage(tile_sheet, 4*64, 3*64, 64, 64,snake[i].x,snake[i].y,box,box); 
+        if(snake.length !=1 && i==snake.length-1 && snake[i-1].x < snake[i].x && snake[i-1].y == snake[i].y ) //going rigth
+            context.drawImage(tile_sheet, 3*64, 3*64, 64, 64,snake[i].x,snake[i].y,box,box); 
+        if(snake.length !=1 && i==snake.length-1 && snake[i-1].x > snake[i].x && snake[i-1].y == snake[i].y ) //going left
+            context.drawImage(tile_sheet, 4*64, 2*64, 64, 64,snake[i].x,snake[i].y,box,box); 
+      
     }
+
 }
 
 function drawFood(){
-    context.fillStyle = "red";
-    context.fillRect(food.x,food.y,box,box)
+    //context.fillStyle = "red";
+    //context.fillRect(food.x,food.y,box,box)
+    context.drawImage(tile_sheet, 0,3*64, 64,64,food.x,food.y,box,box);
 }
 
 document.addEventListener("keydown", update);
@@ -38,9 +105,9 @@ function update(event){
 }
 
 function InitGame(){
-    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x > 16*box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0*box && direction == "left") snake[0].x = 16*box;
-    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y > 16*box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0*box && direction == "up") snake[0].y = 16*box;
 
     for(i=1; i < snake.length;i++){
@@ -60,6 +127,11 @@ function InitGame(){
     if(direction=="left") snakeX -=box;
     if(direction=="up") snakeY -=box;
     if(direction=="down") snakeY +=box;
+
+ //   if(direction=="right") snakeupsection +=1;
+  //  if(direction=="left") snakeupsection +=1;
+   // if(direction=="up") snakeupsection +=1;
+  //  if(direction=="down") snakeupsection +=1;
 
     if (snakeX != food.x || snakeY != food.y){
         snake.pop();
